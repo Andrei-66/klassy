34c79e60b3133084d2d5d0aaf467e43bee1d6785
Andrei-66 <andrei_66@list.ru>
2018-09-26 16:01:35 +0300

    Наконец то все заработало. Осталось оформить

--- src/Vivod_pokazanyi.java
+++ src/Vivod_pokazanyi.java
@@ -1,4 +1,4 @@
-/*import javax.swing.*;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,29 +8,18 @@ import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-class Labels extends JFrame implements ActionListener{
-    Container contantPane=getContentPane();
-    JPanel grid=new JPanel(new GridLayout(4,1));
-    JPanel pnl=new JPanel();
 
-    JLabel lbl=new JLabel("Показания");
+public class Vivod_pokazanyi extends JFrame implements ActionListener  {
+    Container contantPane=getContentPane();
+    JPanel pnl=new JPanel();
+    JPanel grid=new JPanel(new GridLayout(2,2));
+    JButton btn1=new JButton("Внести покзания");
+    JButton btn2=new JButton("Вывести показания");
     JLabel lbl1=new JLabel();
 
-    JTextField text=new JTextField("Текст по умолчанию");
-
-    JButton btn=new JButton("Нажми меня");
-    JButton btn1=new JButton("Вывести данные");
-
-    Color customcolor=new Color(255,0,0);
-
-    Font customFont=new Font("Serif",Font.BOLD,42);
-    Font customFont1=new Font("Serif",Font.BOLD,20);
     String[] month={"январь","февраль","март","апрель","май","июнь","июль",
-                    "август","сентябрь","октябрь","ноябрь","декабрь"};
+            "август","сентябрь","октябрь","ноябрь","декабрь"};
     String[] years={"2011","2012","2013","2014","2015", "2016", "2017"};
-
-    //Формирование массивов для выпадающих списков month и years
-
     JCheckBox mchk1=new JCheckBox(month[0]);
     JCheckBox mchk2=new JCheckBox(month[1]);
     JCheckBox mchk3=new JCheckBox(month[2]);
@@ -55,68 +44,32 @@ class Labels extends JFrame implements ActionListener{
 
 
 
-
-
-    //JLabel lbl2=new JLabel("Это верхний текст","Это нижний текст",JLabel.CENTER);
-
-
-    public Labels(){
+    Vivod_pokazanyi(){
         super("Окно Swing");
         setSize(700,200);
+        pnl.add(new Label(" Показания счетчика"));
 
-        lbl.setOpaque(true);
-        lbl.setBackground(Color.BLACK);
-        lbl.setForeground(Color.WHITE);
-        lbl.setFont(customFont);
-
-        lbl1.setOpaque(true);
-        lbl1.setBackground(Color.BLACK);
-        lbl1.setForeground(Color.WHITE);
-        lbl1.setFont(customFont1);
-
-        btn.addActionListener(this);
-        btn1.addActionListener(this);
-
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
-
-        //lbl.setHorizontalTextPosition(JLabel.CENTER);
-
-        add(pnl);
-
-
-        pnl.add(lbl);
-        pnl.add(lbl1);
-        pnl.add(text);
-        pnl.add(btn);
-        pnl.add(btn1);
-        pnl.add(box1);
-        pnl.add(box2);
-
-
-        grid.add(lbl);
-
-        grid.add(text);
-        grid.add(btn);
-        grid.add(btn1);
+        grid.add( btn1);
         grid.add(box1);
+        grid.add(btn1);
         grid.add(box2);
-        grid.add(lbl1);
+        grid.add(btn2);
 
-        contantPane.add(grid);
-
-
-
+        contantPane.add("North",pnl);
+        contantPane.add("Center", grid);
+        contantPane.add("South",lbl1 );
 
         setVisible(true);
+
+
+
     }
 
     public void actionPerformed(ActionEvent event){
 
         String file="E:\\data\\data.txt";
 
-        if(event.getSource()==btn){
+        if(event.getSource()==btn1){
             try{
                 String line="";
                 FileReader data=new FileReader(file);
@@ -167,12 +120,11 @@ class Labels extends JFrame implements ActionListener{
         }
     }
 
-
-
     public static void main(String[] args) {
-        Labels gui=new Labels();
+
+        Vivod_pokazanyi gui=new Vivod_pokazanyi();
+
+
 
     }
-
 }
-*/
