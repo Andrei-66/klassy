308a83adc1067729fc822dc08c672f9280b9fe14
Andrei-66 <andrei_66@list.ru>
2018-10-08 15:13:17 +0300

    Наконец то все заработало. 28.09.2018 Устранение вывода при нажатии "записать" Добавление тире перед показаниями при выводе.
    Начат новый проект из книги Васильева
    05.10.2018
    продолжение ввода кода из книги Васильева

--- src/TextInputFrame.java
+++ src/TextInputFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 class TextInputFrame implements ActionListener {
@@ -139,6 +140,73 @@ class TextInputFrame implements ActionListener {
         //Класс реализации окна для ввода целого числа
 
         class IntInputFrame implements ActionListener, KeyListener){
+            private JFrame frame;
+            private JLabel enter;
+            prvate JButton btYes,btNo;
+            private JTextField tf;
+            private String name;
+            private int age;
+            //Конструктор класса
+        IntInputFrame(String msg,String name){
+            int width=300,height=150;
+            //Создание окна
+            frame=new JFrame("Окно для ввода (числа)возраста");
+            //Определение положения и размеров окна
+            frame.setBounds(400,300,width,height);
+            //Отключение менеджера компоновки
+            frame.setLayout(null);
+            enter new JLabel(msg,JLabel.LEFT);
+            enter.setBounds(10,10,width-20,height/6);
+            tf new TextField(frame.getWidth()-20);
+            tf.setBounds(10,enter.getHeight()+5,enter.getWidth(),height/6);
+            tf.addKeyListener(this);
+            btYes=new JButton("Подтверждаю");
+            btYes.setBounds(10,height/2+10,(width-30)/2,height/6);
+            btYes.setEnabled(false);
+
+            btNo=new JButton("Завершить");
+            btNo.setBounds((width-30)/2+18,height/2+10,(width-30)/2,height/6);
+            btNo.setFocusPainted(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            //Добавление текстовой метки
+            frame.add(enter);
+            frame.add(tf);
+            frame.add(btYes);
+            frame.add(btNo);
+            frame.setResizable(false);
+
+            btYes.addActionListener(this);
+            btNo.addActionListener(this);
+
+            frame.setVisible(true);
+            //Обработка события отпускания нажатой клавиши
+            public void keyReleased(KeyEvent ke){
+                try {
+                    //Попытка преобьразовать текст в число
+                    Integer.parseInt(tf.getText());
+                    btYes.setEnabled(true);
+
+                }
+
+                catch (Exception e){
+                    btYes.setEnabled(false);
+                }
+                public void keyPressed(KeyEvent ke){}
+                public void keyTyped(KeyEvent ke){}
+
+                //Обработка нажатия кнопки
+                public void actionPerformed(ActionEvent ae){
+                    String btName=ae.getActionCommand();
+
+                }
+
+
+
+            }
+
+
+
+        }
 
         }
 
